e4d658c8849d3cc3f27c532107208a9f2cfdf208
egorka <chernegor@yandex.ru>
2019-03-26 17:29:59 +0300

    add some new paths for ajax querying

--- sea_battle/urls.py
+++ sea_battle/urls.py
@@ -4,12 +4,18 @@ from sea_battle.views import \
                                 HelloView, \
                                 SeeUsersView, \
                                 GameNewView, \
-                                GamePlayView
+                                GameJoinView, \
+                                GamePlayView, \
+                                AwaitedFleetView, \
+                                CleaningView
 
 urlpatterns = [
     path('', HelloView.as_view(), name='index'),
     path('accounts/signup/', RegisterFormView.as_view(), name='signup'),
     path('accounts/logged_in/', SeeUsersView.as_view(), name='see_users'),
     path('game_create/', GameNewView.as_view(), name='game_new'),
+    path('game_join/', GameJoinView.as_view(), name='game_join'),
     path('game/', GamePlayView.as_view(), name='gameplay'),
+    path('awaited_fleet/', AwaitedFleetView.as_view(), name='awaited_fleet'),
+    path('cleaning_db/', CleaningView.as_view(), name='deleting_fleet')
 ]
