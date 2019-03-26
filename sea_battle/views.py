1c4f6717a39419b600d20e835ae172efcd8b43c9
egorka <chernegor@yandex.ru>
2019-03-26 17:35:02 +0300

    added 3 new classviews. 1st for cleaning database after game is over, 2nd for answer if opponent is come

--- sea_battle/views.py
+++ sea_battle/views.py
@@ -4,8 +4,7 @@ import json
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.core import serializers
-from django.db.models import F
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -14,6 +13,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import FormView, TemplateView
 import online_users.models
 from datetime import timedelta
+from django.db import models
 from sea_battle.forms import GameAttrForm
 from sea_battle.models import BattleMap
 
@@ -59,11 +59,18 @@ class SeeUsersView(FormView):
     def get(self, request, *args, **kwargs):
         user_status = online_users.models.OnlineUserActivity.get_user_activities(timedelta(seconds=60))
         users = (user for user in user_status)
+        cleared_users = []
+        # preventing view self username in available games list:
+        for user in users:
+            if user.user == request.user:
+                continue
+            else:
+                cleared_users.append(user.user)
         return render(
             request,
             SeeUsersView.template_name,
             context={
-                 "users": users,
+                 "online_users": cleared_users,
             }
         )
 
@@ -90,10 +97,40 @@ class GameNewView(FormView):
         )
 
 
+class GameJoinView(FormView):
+
+    template_name = 'pre_battle.html'
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+
+        opponent = request.POST.get('opponent_username')
+        username_id = User.objects.get(username=opponent).id
+
+        # retrieving size of created game
+
+        size = BattleMap.objects.get(user=username_id).map_of_bf['size']
+
+        # set joined username as opponent at user, who has created game
+
+        tmp = BattleMap.objects.get(user=username_id).map_of_bf
+        tmp['opponent_username'] = request.user.username
+        BattleMap.objects.filter(user=username_id).update(map_of_bf=tmp)
+        sizeiterator = list(range(int(size)))
+
+        return render(
+            request,
+            GameNewView.template_name,
+            context={
+                'size': size,
+                'sizeiterator': sizeiterator,
+                'opponent': opponent,
+            }
+        )
+
 class GamePlayView(FormView):
 
     template_name = 'battle.html'
-    # https://www.findclip.net/video/oNhNzH8FCIM/7-uroki.html  form custom validation
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
@@ -102,38 +139,71 @@ class GamePlayView(FormView):
             form = GameAttrForm({
                 'map_of_bf': request.POST.get('json_form')
             })
-
             if form.is_valid():
 
                 print('form of ',request.user,' is ok')
-
 
                 if not BattleMap.objects.filter(user=request.user):
                     battlemap = form.save(commit=False)
                     battlemap.user = request.user
                     battlemap.save()
-                    print("Created!")
+                    print("map1 Created!")
                 else:
                     battlemap = form.save(commit=False)
                     battlemap.user = request.user
-                    # battlemap.save()
                     tmp = BattleMap.objects.filter(user=request.user)
                     tmp.update(map_of_bf=form.cleaned_data['map_of_bf'])
-                    print("Updated!")
+                    print("map1 Updated!")
             else:
-
                 print('form is invalid: ',form.errors)
 
             respjson = BattleMap.objects.get(user=request.user).map_of_bf
+            opponent = respjson['opponent_username']
             sizeiterator = list(range(int(respjson['size'])))
             data = json.dumps(respjson)
+            try:
+                username_id = User.objects.get(username=opponent).id
+                joined_resp = BattleMap.objects.get(user=username_id).map_of_bf
+                joined_resp = json.dumps(joined_resp)
+            except:
+                joined_resp = {}
             return render(
                 request,
                 GamePlayView.template_name,
 
                 context={
-                    'form': data,
+                    'form':         data,
                     'sizeiterator': sizeiterator,
-                    'size': respjson['size'],
+                    'size':         respjson['size'],
+                    'opponent':     opponent,
+                    'joined_map':   joined_resp
                 }
             )
+
+
+class AwaitedFleetView(FormView):
+
+    # class checks if enemy's fleet has arrived and gives
+    # an answer to battle.html via ajax
+    # class is being used by player, who creates game
+
+    def get(self,request, *args, **kwargs):
+        opponent = BattleMap.objects.get(user=request.user).map_of_bf['opponent_username']
+        print("Waiting for a fleet of ", opponent, " by ", request.user)
+        try:
+            username_id = User.objects.get(username=opponent).id
+            respjson2 = BattleMap.objects.get(user=username_id).map_of_bf
+        except:
+            respjson2 = {}
+        return JsonResponse(respjson2)
+
+
+class CleaningView(FormView):
+
+    # deleting map of player, when the window is being hidden by him
+
+    def get(self, request, *args, **kwargs):
+        BattleMap.objects.filter(user=request.user).delete()
+        print("Map of ", request.user, " deleted from db")
+        resp = {"window.onclose event": "Deleting map successfully completed"}
+        return JsonResponse(resp)
